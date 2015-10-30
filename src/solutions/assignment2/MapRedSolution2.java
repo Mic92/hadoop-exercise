e1d2fc1cf0ed09ebafff3b82880147c4dc00ccab
Jörg Thalheim <joerg@higgsboson.tk>
2015-10-30 08:07:10 +0100

    add Solution 2

--- src/solutions/assignment2/MapRedSolution2.java
+++ src/solutions/assignment2/MapRedSolution2.java
@@ -1,17 +1,50 @@
 package solutions.assignment2;
 
+import examples.dns.DNSFileInputFormat;
+import examples.dns.DNSRecordIO;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 import examples.MapRedFileUtils;
+import org.xbill.DNS.Type;
+
+import java.io.IOException;
+import java.util.Iterator;
 
 public class MapRedSolution2
 {
-    /* your code goes in here*/
+    public static class ExtractIps extends Mapper<Text, DNSRecordIO, Text, NullWritable> {
+        @Override
+        protected void map(Text key, DNSRecordIO record, Mapper.Context context) throws IOException, InterruptedException {
+            if (record.getType().get() == Type.A || record.getType().get() == Type.AAAA) {
+                context.write(record.getRdata(), NullWritable.get());
+            }
+        }
+    }
+
+    public static class TextFrequency extends Reducer<Text, NullWritable, Text, LongWritable> {
+        private final LongWritable freq = new LongWritable(0);
+        @Override
+        protected void reduce(Text key, Iterable<NullWritable> values, Context context) throws IOException, InterruptedException {
+            long i = 0;
+            Iterator iter = values.iterator();
+            while(iter.hasNext()) {
+                iter.next();
+                i++;
+            }
+            freq.set(i);
+            context.write(key, freq);
+        }
+    }
 
     public static void main(String[] args) throws Exception
     {
@@ -27,11 +60,17 @@ public class MapRedSolution2
         }
 
         Job job = Job.getInstance(conf, "MapRed Solution #2");
-
-        /* your code goes in here*/
+        job.setInputFormatClass(DNSFileInputFormat.class);
 
         FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
         FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+
+        job.setMapperClass(ExtractIps.class);
+        job.setReducerClass(TextFrequency.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(NullWritable.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
 
         MapRedFileUtils.deleteDir(otherArgs[1]);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
