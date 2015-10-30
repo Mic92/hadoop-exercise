2feed36db80c8c16c4bb7a14d89c52f61d3ffa16
Jörg Thalheim <joerg@higgsboson.tk>
2015-10-30 09:37:45 +0100

    add solution 4

--- src/solutions/assignment4/MapRedSolution4.java
+++ src/solutions/assignment4/MapRedSolution4.java
@@ -1,17 +1,35 @@
 package solutions.assignment4;
 
+import examples.dns.DNSFileInputFormat;
+import examples.dns.DNSRecordIO;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 import examples.MapRedFileUtils;
+import org.xbill.DNS.Type;
+import solutions.assignment3.MapRedSolution3;
+
+import java.io.IOException;
 
 public class MapRedSolution4
 {
-    /* your code goes in here*/
+    public static class ExtractCnameWithIPs extends Mapper<Text, DNSRecordIO, Text, NullWritable> {
+        @Override
+        protected void map(Text key, DNSRecordIO record, Mapper.Context context) throws IOException, InterruptedException {
+            final String rdata = record.getRdata().toString();
+            // one does not simply parse inet addresses with regex
+            if (record.getType().get() == Type.CNAME && InetAddresses.isInetAddress(rdata)) {
+                context.write(new Text(record.getRawRecord().toString()), NullWritable.get());
+            }
+        }
+    }
 
     public static void main(String[] args) throws Exception
     {
@@ -28,7 +46,14 @@ public class MapRedSolution4
 
         Job job = Job.getInstance(conf, "MapRed Solution #4");
 
-        /* your code goes in here*/
+        job.setInputFormatClass(DNSFileInputFormat.class);
+
+        job.setMapperClass(ExtractCnameWithIPs.class);
+        job.setReducerClass(MapRedSolution3.WriteThrough.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(NullWritable.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(NullWritable.class);
 
         FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
         FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
