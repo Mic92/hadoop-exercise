3eb1f3f2d080a04a3234384b5934ecea6900199b
Jörg Thalheim <joerg@higgsboson.tk>
2015-10-30 08:19:42 +0100

    add solution 3

--- src/solutions/assignment3/MapRedSolution3.java
+++ src/solutions/assignment3/MapRedSolution3.java
@@ -1,17 +1,42 @@
 package solutions.assignment3;
 
+import examples.dns.DNSFileInputFormat;
+import examples.dns.DNSRecordIO;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 import examples.MapRedFileUtils;
+import org.xbill.DNS.Type;
+
+import java.io.IOException;
 
 public class MapRedSolution3
 {
-    /* your code goes in here*/
+    public static class ExtractCnameIdentity extends Mapper<Text, DNSRecordIO, Text, NullWritable> {
+        @Override
+        protected void map(Text key, DNSRecordIO record, Mapper.Context context) throws IOException, InterruptedException {
+            if (record.getType().get() == Type.CNAME) {
+                if (record.getName().equals(record.getRdata())) {
+                    context.write(new Text(record.getRawRecord().toString()), NullWritable.get());
+                }
+            }
+        }
+    }
+
+    public static class WriteThrough extends Reducer<Text, NullWritable, Text, NullWritable> {
+        @Override
+        protected void reduce(Text key, Iterable<NullWritable> values, Context context) throws IOException, InterruptedException {
+            context.write(key, NullWritable.get());
+        }
+    }
 
     public static void main(String[] args) throws Exception
     {
@@ -28,7 +53,17 @@ public class MapRedSolution3
 
         Job job = Job.getInstance(conf, "MapRed Solution #3");
 
-        /* your code goes in here*/
+        job.setInputFormatClass(DNSFileInputFormat.class);
+
+        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+
+        job.setMapperClass(ExtractCnameIdentity.class);
+        job.setReducerClass(WriteThrough.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(NullWritable.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(NullWritable.class);
 
         FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
         FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
